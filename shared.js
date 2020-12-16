b77d09c2ed535a42adc73967db2a8ae527644cdb
muko <m_vadachkoria@cu.edu.ge>
2020-12-16 16:41:39 +0400

    support query

--- shared.js
+++ shared.js
@@ -15,7 +15,10 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     // backdrop.style.display = "block";
     // modal.className = 'open'; // This will actually overwrite the complete class list
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
@@ -36,24 +39,28 @@ function closeModal() {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", function() {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10);
 });
 
 ctaButton.addEventListener('animationstart', function(event) {
   console.log('Animation started', event);
 })
 
-
 ctaButton.addEventListener('animationend', function(event) {
   console.log('Animation ended', event);
 })
-
 
 ctaButton.addEventListener('animationiteration', function(event) {
   console.log('Animation iteration', event);
